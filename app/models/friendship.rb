620fe6f67a29f011d47b536addedaa00773cabdc
Pierre Colle <piercus@dhcp-24-150.wifi-auth.ecp.fr>
2010-03-03 15:17:30 +0100

    bug fix in friendship.rb

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,4 @@
 class Friendship < ActiveRecord::Base
-  belongs_to :user_to, :foreign_key => "user_to", :class_name => "User"
-  belongs_to :user_from, :foreign_key => "user_from", :class_name => "User"
-
+  belongs_to :user_to, :foreign_key => "user_to_id", :class_name => "User"
+  belongs_to :user_from, :foreign_key => "user_from_id", :class_name => "User"
 end
