bfcf0a25097f21ad64a3c59a57c2d0d439902b49
guillaumegaluz <guillaume.galuz@gmail.com>
2010-03-11 20:43:11 +0100

    controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,25 @@ class WelcomeController < ApplicationController
   end
   
   def search
-    if params[:id_user]
-      @user = User.find(:first, :conditions => ["screen_name = ?", params[:id_user]])
-    else
-      @user = User.find(:first, :conditions => ["screen_name = ?", params["user"]["username"]])
-    end
-    @tweet0 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[0]
-    @tweet1 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[1]
-    @tweet2 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[2]
     
-  end
-  
+    if params[:type]=="valueuser"
+      if params[:id_user].blank?
+        flash[:notice] = "User not valid"
+        redirect_to :action => "index"
+      else
+        @user = User.find(:first, :conditions => ["screen_name = ?", params[:id_user]])
+        @last_tweets = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[-3..-1]
+      end  
+    elsif params[:type]=="valuetag"
+      if params[:id_tag].blank?
+        flash[:notice] = "Tag not valid"
+        redirect_to :action => "index"
+      else
+        @tag = Tag.find(:first, :conditions => ["word = ?", params[:id_tag]])
+        redirect_to :action => "index"
+      end
+    end
+    
+  end      
+    
 end
