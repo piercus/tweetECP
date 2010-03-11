5dc83b8658955779c9d06585669f1505a294d47a
guillaumegaluz <guillaume.galuz@gmail.com>
2010-03-11 15:15:11 +0100

    search views

--- app/controllers/_backup_users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  
-  def index
-    @users = User.find(:all, :conditions => ['name LIKE ?', "%#{params[:search]}%"])
-  end
-  
-end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,16 +3,20 @@ class WelcomeController < ApplicationController
   def index
   end
   
-  def search
-    if params[:id]
-      @user = User.find(:first, :conditions => ["screen_name = ?", params[:id]])
-    else
-      @user = User.find(:first, :conditions => ["screen_name = ?", params["user"]["username"]]) 
-    end 
+  def hello
+    @users = User.find(:all)
   end
   
-  def hello
-    @users = User.find(:all) 
+  def search
+    if params[:id_user]
+      @user = User.find(:first, :conditions => ["screen_name = ?", params[:id_user]])
+    else
+      @user = User.find(:first, :conditions => ["screen_name = ?", params["user"]["username"]])
+    end
+    @tweet0 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[0]
+    @tweet1 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[1]
+    @tweet2 = Tweet.find(:all,:conditions => ["user_id = ?", @user.id])[2]
+    
   end
   
 end
