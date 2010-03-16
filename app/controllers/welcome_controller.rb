9fccf30ab988da1a69306a9798896472cd3138ae
Colle <piercus@gmail.com>
2010-03-16 16:23:04 +0100

    Change in views and models

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,21 +1,30 @@
 class WelcomeController < ApplicationController
+  protect_from_forgery :only => [:create, :update, :destroy] 
+  def index	       
+	url = url_for(:controller => "welcome", :action => "search", :type => "valueuser", :id_user => "scobleizer")
   
-  def index
+	 @input = {"tags" => {"web" => [10,""], "music" => [20,""], "politics" => [20,""], "sports" => [20,""]}, "users" => {"loic" => [20,""], "scobleizer" => [20,url], "jalove" => [20,""], "thaven" => [20,""]} }.to_json
+
   end
-      
+  def autocomplete
+    return false if !params[:query]
+		like = params[:query].concat("%")
+
+		users = User.find(:all,:conditions => ["screen_name like ?", like]).collect{|u| u.screen_name}
+		render :text => users.to_json
+	
+	end    
   def search
 
     
     url = url_for(:controller => "welcome", :action => "search", :type => "valueuser", :id_user => "scobleizer")
     @input = {"tags" => {"web" => [10,""], "music" => [20,""], "politics" => [20,""], "sports" => [20,""]}, "users" => {"loic" => [20,""], "scobleizer" => [20,url], "jalove" => [20,""], "thaven" => [20,""]} }.to_json
 
-    
-
     reco = { 
 		   "users" => User.recomand(params[:type],params[:id]),
 		   "tags" => Tag.recomand(params[:type],params[:id])
 		}
-    if params[:type]=="User"
+    if params[:type]=="user"
 		  
 
       if params[:id].blank?
@@ -24,11 +33,11 @@ class WelcomeController < ApplicationController
       else
 
        # @user = User.find(:first, :conditions => ["screen_name = ?", params[:id]])
-        @user = User.find(:first, :conditions => ["screen_name = ?", params[:id_user]])
-        @last_tweets = @user.tweets[-3..-1]
+        @user = User.find(:first, :conditions => ["screen_name = ?", params[:id]])
+        @last_tweets = @user.tweets
         #@last_links = Link.find(:all,:conditions => ["user_id = ?", @user.id])[-5..-1]
       end  
-    elsif params[:type]=="Tag"
+    elsif params[:type]=="tag"
       if params[:id].blank?
         flash[:notice] = "Tag not valid"
         redirect_to :action => "index"
