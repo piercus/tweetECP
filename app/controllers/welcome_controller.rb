1c8096e432903734c7b5b8d7f7dbe1a0af23ae9c
Colle <piercus@gmail.com>
2010-03-17 18:47:16 +0100

    change welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,20 @@
 class WelcomeController < ApplicationController
   protect_from_forgery :only => [:create, :update, :destroy] 
+	before_filter :set_class_object :except => :index
+	
+	#Cette action va être lu avant les autres actions et va permettre de faire des actions communes à toutes les action s(ici il s'agit de savoir si on fait du Tag ou du User)
+	def set_class_object
+	   if params[:type]=="user"
+		  classType = User
+		elsif params[:type]=="tag"
+		  classType = Tag
+		else
+		  raise "Not a valid type"
+			return false
+		end
+	end
   def index	       
-	url = url_for(:controller => "welcome", :action => "search", :type => "valueuser", :id_user => "scobleizer")
+	  url = url_for(:controller => "welcome", :action => "search", :type => "valueuser", :id_user => "scobleizer")
   
 	 @input = {"tags" => {"web" => [10,""], "music" => [20,""], "politics" => [20,""], "sports" => [20,""]}, "users" => {"loic" => [20,""], "scobleizer" => [20,url], "jalove" => [20,""], "thaven" => [20,""]} }.to_json
 
@@ -9,43 +22,26 @@ class WelcomeController < ApplicationController
   def autocomplete
     return false if !params[:query]
 		like = params[:query].concat("%")
-
-		users = User.find(:all,:conditions => ["screen_name like ?", like]).collect{|u| u.screen_name}
+		object = classType.find_by_dname_like(like).collect{|u| u.dname}
 		render :text => users.to_json
-	
 	end    
   def search
-
-    
-    url = url_for(:controller => "welcome", :action => "search", :type => "valueuser", :id_user => "scobleizer")
-    @input = {"tags" => {"web" => [10,""], "music" => [20,""], "politics" => [20,""], "sports" => [20,""]}, "users" => {"loic" => [20,""], "scobleizer" => [20,url], "jalove" => [20,""], "thaven" => [20,""]} }.to_json
-
-    reco = { 
-		   "users" => User.recomand(params[:type],params[:id]),
-		   "tags" => Tag.recomand(params[:type],params[:id])
-		}
-    if params[:type]=="user"
-		  
-
-      if params[:id].blank?
-        flash[:notice] = "User not valid"
+		if params[:id].blank?
+        flash[:notice] = "Not a valid "+params[:type]
         redirect_to :action => "index"
-      else
-
-       # @user = User.find(:first, :conditions => ["screen_name = ?", params[:id]])
-        @user = User.find(:first, :conditions => ["screen_name = ?", params[:id]])
-        @last_tweets = @user.tweets
-        #@last_links = Link.find(:all,:conditions => ["user_id = ?", @user.id])[-5..-1]
-      end  
-    elsif params[:type]=="tag"
-      if params[:id].blank?
-        flash[:notice] = "Tag not valid"
+				return false
+		end
+    @object = classType.find_by_dname(params[:id])	
+  	if @object.nil?
+        flash[:notice] = "Not a valid "+params[:type]
         redirect_to :action => "index"
-      else
-        @tag = Tag.find(:first, :conditions => ["word = ?", params[:id]])
-      end
-    end
-    
+				return false
+		end
+		@last_tweets = @object.tweets
+    @input = {
+ 		   "users" => @object.get_best_users(10,1),
+		   "tags" => @object.get_best_tags(10,1,)
+		}    
   end      
-    
+	
 end
